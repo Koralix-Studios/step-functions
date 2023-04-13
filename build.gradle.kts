fbded1aa3cb2ba4e1b38cf2301d77abaacb521d4
JohanVonElectrum <joanestradium@gmail.com>
2023-04-13 05:22:40 +0200

    fix try of ossrh publishing CI/CD workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,8 +83,8 @@ publishing {
             name = "sonatype"
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
+                username = System.getenv("OSSRH_USERNAME") ?: project.findProperty("ossrh.user") as String?
+                password = System.getenv("OSSRH_PASSWORD") ?: project.findProperty("ossrh.password") as String?
             }
         }
     }
